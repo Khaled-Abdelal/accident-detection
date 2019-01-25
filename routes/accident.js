5438c362176e35e1e3fd8f83baa6c51073425240
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-01-25 22:28:47 +0200

    added search for hospitals in 100km distance  to send the accident data to

--- routes/accident.js
+++ routes/accident.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const Hospital = require("../models/Hospital");
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const location = req.body.location;
-  console.log("route hit location " + location + " io " + req.io.sockets);
 
-  req.io.sockets
-    .in("5c4245caf4873a5d5aecf057")
-    .emit("accident", { location: location });
+  console.log("route hit location " + location + " io " + req.io.sockets);
+  const hospitals = await Hospital.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: "Point",
+          coordinates: [
+            parseFloat(req.body.location[0]),
+            parseFloat(req.body.location[1])
+          ]
+        },
+        spherical: true,
+        maxDistance: 100000,
+        distanceField: "dist.calculated"
+      }
+    }
+  ]);
+  hospitals.forEach(hospital => {
+    console.log(hospital._id);
+    req.io.sockets.in(hospital._id).emit("accident", { location: location });
+  });
+
   res.end();
 });
 
