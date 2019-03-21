beb1d2853fc1b23654e9851034be54b5baba2035
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-03-21 20:42:50 +0200

    add env google api key to backend prod file

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   JWT_KEY: process.env.JWT_KEY,
-  JWT_USER_KEY: process.env.JWT_USER_KEY
+  JWT_USER_KEY: process.env.JWT_USER_KEY,
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
 };
